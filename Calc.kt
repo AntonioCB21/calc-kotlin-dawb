aed8ac7b7030faa8fbc90cb2c77aea2cef6def62
AntonioCB21 <cbantonio@iesrafaelalberti.es>
2025-03-12 17:34:45 +0100

    Función división sin error

--- Calc.kt
+++ Calc.kt
@@ -2,7 +2,7 @@ fun main(args: Array) {
     val (operation, num1, num2) = parseArguments(args)
     val result = when (operation) {
         "multiplica" -> multiplica(num1, num2)
-	"divide" -> divideE(num1, num2)
+	"divide" -> divide(num1, num2)
         else -> throw IllegalArgumentException("Operación no soportada")
     }
     showResult(operation, result)
@@ -22,7 +22,10 @@ fun multiplica(num1: Int, num2: Int): Int {
     return num1 * num2
 }
 
-fun divideE(num1: Int, num2: Int): Int {
+fun divide(num1: Int, num2: Int): Int {
+    if (num2 == 0) {
+        throw IllegalArgumentException("Error: No se puede dividir por cero")
+    }
     return num1 / num2
 }
 
